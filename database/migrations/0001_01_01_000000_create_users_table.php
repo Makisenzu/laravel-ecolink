9bd4f4d9fa94fad4076d05ac7a2bf8c7a7f3c6ba
Denmark B Rivera <denmarkbarbarona13@gmail.com>
2026-04-19 17:54:13 +0800

    feat: new migration, model, relationship

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->foreignId('role_id')->constrained('roles')->cascadeOnDelete();
+            $table->string('firstname');
+            $table->string('middlename')->nullable();
+            $table->string('lastname')->nullable();
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');

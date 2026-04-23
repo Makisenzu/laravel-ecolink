e2aad5d2d42553a79ad5107827c70857aa31c439
Denmark B Rivera <denmarkbarbarona13@gmail.com>
2026-04-24 02:25:10 +0800

    feat: added drivers table MM

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
     protected function role() {
         return $this->belongsTo(Role::class);
     }
+
+        public function driver()
+        {
+            return $this->hasMany(Driver::class);
+        }
 }

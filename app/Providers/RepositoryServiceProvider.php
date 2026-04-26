0928fbca7bf6a0dc5e32b5bcc17369c174cfe00c
Denmark B Rivera <denmarkbarbarona13@gmail.com>
2026-04-26 18:14:36 +0800

    feat: new collection queue repositories

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
+use App\Repositories\Eloquent\CollectionQueueRepository;
 use App\Repositories\Eloquent\DriverRepository;
 use App\Repositories\Eloquent\ScheduleRepository;
 use App\Repositories\Eloquent\SiteRepository;
+use App\Repositories\Interfaces\CollectionQueueRepositoryInterface;
 use App\Repositories\Interfaces\DriverRepositoryInterface;
 use App\Repositories\Interfaces\ScheduleRepositoryInterface;
 use App\Repositories\Interfaces\SiteRepositoryInterface;
@@ -20,6 +22,7 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(SiteRepositoryInterface::class, SiteRepository::class);
         $this->app->bind(DriverRepositoryInterface::class, DriverRepository::class);
         $this->app->bind(ScheduleRepositoryInterface::class, ScheduleRepository::class);
+        $this->app->bind(CollectionQueueRepositoryInterface::class, CollectionQueueRepository::class);
     }
 
     /**

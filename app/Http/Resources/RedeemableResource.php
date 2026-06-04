fd3b25fc772d107e9785df67c7f753d2b5f4e424
Denmark B Rivera <denmarkbarbarona13@gmail.com>
2026-06-04 16:52:34 +0800

    feat: added new resource for redeemables

---
+++ app/Http/Resources/RedeemableResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RedeemableResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'Category' =>$this->redeemable_category_id->category_name,
+            'Item Name' => $this->item_name,
+            'Description' => $this->description,
+            'Points Required' => $this->points_required,
+            'Stock' => $this->stock,
+            'Status' => $this->status
+        ];
+    }
+}
